9d6e069d42c679fec461acd8c46d16e718ecbe07
Temi Priscilla Jokotola <153321707+temidataspot@users.noreply.github.com>
2025-08-25 14:16:50 +0100

    Update and rename Analytical Solutions.md to PearsonCorrelation.R

--- PearsonCorrelation.R
+++ PearsonCorrelation.R
@@ -1,21 +1,6 @@
-# Analysis 
-
-The purpose of this analysis is to investigate patterns of antibiotic co-resistance among the ten antibiotics. 
-By examining correlations between resistance levels, the analysis aims to identify if resistance to one antibiotic is 
-statistically associated with resistance to others. This can reveal patterns of multidrug resistance, which occur when 
-bacteria are resistant to multiple antibiotics due to shared mechanisms or selective pressures.
-
-**Steps:**
-- Install relevant packages and load their libraries
-- Load the uploaded excel file and assign to a dataframe
-- Subset the columns related to antibiotic resistance
-- Calculate correlation matrix. Method used in this analysis is **pearson correlation**
-- Plot correlation heatmap
-
-# Pearson correlation
-
 ```r
-# Install the necessary packages
+# PEARSON CORRELATION
+# Installing the necessary packages
 install.packages("readxl")
 install.packages("tidyverse")
 install.packages("dplyr")
@@ -26,7 +11,7 @@ install.packages("pheatmap")
 install.packages("RColorBrewer")
 install.packages("ggcorrplot")
 
-#Load libraries
+# load libraries
 library(readxl)
 library(tidyverse)
 library(dplyr)
@@ -68,5 +53,6 @@ correlation_matrix_combined <- cor(antibiotic_combined, method = "pearson")
 ggcorrplot(correlation_matrix_combined, method = "circle", colors = c("purple", "white", "red"), lab = TRUE) +
   ggtitle("Correlation of Antibiotic Resistance") +
   theme(plot.title = element_text(hjust = 0.5))  # Center the title
-```
+
 ![image](https://github.com/temidataspot/Correlation-Analysis-of-Antibiotic-Resistance/blob/main/Screenshot%202024-10-18%20113043.png)
+```
